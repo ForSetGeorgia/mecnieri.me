ee4df833169e051eb0e142d34c00a3996bb3d9bc
jason <jason.addie@gmail.com>
2016-08-12 15:42:49 +0400

    seeing if folder name is prob with puma

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 set :domain, 'alpha.jumpstart.ge'
 set :user, 'metsnieri-staging'
-set :application, 'Metsnieri.me'
+set :application, 'Metsnieri'
 # easier to use https; if you use ssh then you have to create key on server
 set :repository, 'https://github.com/JumpStartGeorgia/metsnieri.me.git'
 set :branch, 'master'
