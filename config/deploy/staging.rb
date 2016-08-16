e842b141183e5b8cac331eceab0abaa332c0d607
jason <jason.addie@gmail.com>
2016-08-16 09:44:28 +0400

    fix nginx config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,6 +4,6 @@ set :application, 'Metsnieri'
 # easier to use https; if you use ssh then you have to create key on server
 set :repository, 'https://github.com/JumpStartGeorgia/metsnieri.me.git'
 set :branch, 'master'
-set :web_url, 'dev-metsnieri.jumsptart.ge'
+set :web_url, 'dev-metsnieri.jumpstart.ge'
 set :visible_to_robots, false
 set :use_ssl, false
