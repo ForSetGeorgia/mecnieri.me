e1b08957bc30458b19cdac03896dfdd84b855a59
jason <jason.addie@gmail.com>
2016-08-22 16:44:26 +0400

    re-arranged exp form fields; added missing formtastic locales

--- app/assets/javascripts/admin/experiments.js
+++ app/assets/javascripts/admin/experiments.js
@@ -8,7 +8,7 @@ function updateTableIndex(tables){
 }
 
 // move a row in other tables to match the row that was just moved
-function moveRowInOtherTables(current_tab_pane, original_index, move_direction){
+function moveRowInOtherTables(current_tab_pane, table_data_type, original_index, move_direction){
   var $tab_panes = current_tab_pane.closest('.js-cocoon').find('.tab-pane');
   var original_locale = current_tab_pane.data('locale');
 
@@ -16,8 +16,8 @@ function moveRowInOtherTables(current_tab_pane, original_index, move_direction){
   $tab_panes.each(function(){
     // if the tab pane is not the original locale that already had the row moved, move it
     if ($(this).data('locale') != original_locale){
-      var $rows = $(this).find('table tbody tr');
-      var $row_to_move = $(this).find('table tbody tr:eq(' + original_index + ')');
+      var $rows = $(this).find('table[data-type="' + table_data_type + '"] tbody tr');
+      var $row_to_move = $(this).find('table[data-type="' + table_data_type + '"] tbody tr:eq(' + original_index + ')');
       if ($row_to_move.length > 0){
         if (move_direction == 'up'){
           $row_to_move.insertBefore($rows[original_index-1]);
@@ -34,7 +34,7 @@ function moveRowInOtherTables(current_tab_pane, original_index, move_direction){
 
 function setupExperimentCocoon(){
   // add ingredient
-  $('.js-cocoon-ingredients .tab-pane').on('cocoon:after-insert', function(e, insertedItem) {
+  $('.js-cocoon .tab-pane').on('cocoon:after-insert', 'table.table-ingredients', function(e, insertedItem) {
     var locale = $(this).data('locale');
     var page_locales = [];
     var row = $(insertedItem).clone();
@@ -43,7 +43,7 @@ function setupExperimentCocoon(){
     console.log(locale);
 
     // get all locales except the active one
-    $('.js-cocoon-ingredients .tab-pane').each(function(){if ($(this).data('locale') != locale){page_locales.push($(this).data('locale'))}});
+    $('.js-cocoon .tab-pane').each(function(){if ($(this).data('locale') != locale){page_locales.push($(this).data('locale'))}});
 
     console.log(page_locales);
 
@@ -52,20 +52,20 @@ function setupExperimentCocoon(){
       // update the inserted row with the appropriate locale
       $(row).html($(row).html().replace(new RegExp("_" + locale,"g"), '_' + page_locales[i]));
       // add the row
-      $('.js-cocoon-ingredients .tab-pane[data-locale="' + page_locales[i] + '"] table tbody').append($(row));
+      $('.js-cocoon .tab-pane[data-locale="' + page_locales[i] + '"] table.table-ingredients tbody').append($(row));
     }
 
     // update the row directions
-    updateTableIndex($(this).closest('.js-cocoon').find('.tab-pane table tbody'));
+    updateTableIndex($(this).closest('.js-cocoon').find('.tab-pane table.table-ingredients tbody'));
   });
 
   // delete ingredient
-  $('.js-cocoon-ingredients .tab-pane').on('cocoon:before-remove', function(e, deletedItem) {
+  $('.js-cocoon .tab-pane').on('cocoon:before-remove', 'table.table-ingredients', function(e, deletedItem) {
     // get the index of the row being deleted so the same row for other languages can also be deleted
-    var index = $(this).find('table tbody tr').index(deletedItem);
+    var index = $(this).find('table.table-ingredients tbody tr').index(deletedItem);
 
     // mark this row in every table as deleted
-    $('.js-cocoon-ingredients .tab-pane table tbody').each(function(){
+    $('.js-cocoon .tab-pane table.table-ingredients tbody').each(function(){
       var row = $(this).find('tr')[index];
       // mark as deleted
       $(row).find('td:last input').val('true');
@@ -76,33 +76,33 @@ function setupExperimentCocoon(){
   });
 
   // add direction
-  $('.js-cocoon-directions .tab-pane').on('cocoon:after-insert', function(e, insertedItem) {
+  $('.js-cocoon .tab-pane').on('cocoon:after-insert', 'table.table-directions', function(e, insertedItem) {
     var locale = $(this).data('locale');
     var page_locales = [];
     var row = $(insertedItem).clone();
 
     // get all locales except the active one
-    $('.js-cocoon-directions .tab-pane').each(function(){if ($(this).data('locale') != locale){page_locales.push($(this).data('locale'))}});
+    $('.js-cocoon .tab-pane').each(function(){if ($(this).data('locale') != locale){page_locales.push($(this).data('locale'))}});
 
     // replace the locale and then add the row to the correct table
     for(var i=0;i<page_locales.length;i++){
       // update the inserted row with the appropriate locale
       $(row).html($(row).html().replace(new RegExp("_" + locale,"g"), '_' + page_locales[i]));
       // add the row
-      $('.js-cocoon-directions .tab-pane[data-locale="' + page_locales[i] + '"] table tbody').append($(row));
+      $('.js-cocoon .tab-pane[data-locale="' + page_locales[i] + '"] table.table-directions tbody').append($(row));
     }
 
     // update the row directions
-    updateTableIndex($(this).closest('.js-cocoon').find('.tab-pane table tbody'));
+    updateTableIndex($(this).closest('.js-cocoon').find('.tab-pane table.table-directions tbody'));
   });
 
   // delete direction
-  $('.js-cocoon-directions .tab-pane').on('cocoon:before-remove', function(e, deletedItem) {
+  $('.js-cocoon .tab-pane').on('cocoon:before-remove', 'table.table-directions', function(e, deletedItem) {
     // get the direction of the row being deleted so the same row for other languages can also be deleted
-    var index = $(this).find('table tbody tr').index(deletedItem);
+    var index = $(this).find('table.table-directions tbody tr').index(deletedItem);
 
     // mark this row in every table as deleted
-    $('.js-cocoon-directions .tab-pane table tbody').each(function(){
+    $('.js-cocoon .tab-pane table.table-directions tbody').each(function(){
       var row = $(this).find('tr')[index];
       // mark as deleted
       $(row).find('td:last input').val('true');
@@ -118,6 +118,7 @@ function setupExperimentMove(){
 
   $('tr.nested-fields a.move-up').on('click', function(){
     var $row = $(this).closest('tr');
+    var $data_type = $row.closest('table').data('type')
 
     // cannot use jquery next for cocoon puts hidden fields in between
     // tr tags, so have to look for next by hand
@@ -129,16 +130,17 @@ function setupExperimentMove(){
 
 
     // make the same movement in the other tables
-    moveRowInOtherTables($row.closest('.tab-pane'), index, 'up');
+    moveRowInOtherTables($row.closest('.tab-pane'), $data_type, index, 'up');
 
     // update the row directions
-    updateTableIndex($(this).closest('.js-cocoon').find('.tab-pane table tbody'));
+    updateTableIndex($(this).closest('.js-cocoon').find('.tab-pane table[data-type="' + $data_type + '"] tbody'));
 
     return false;
   });
 
   $('tr.nested-fields a.move-down').on('click', function(){
     var $row = $(this).closest('tr');
+    var $data_type = $row.closest('table').data('type')
 
     // cannot use jquery next for cocoon puts hidden fields in between
     // tr tags, so have to look for next by hand
@@ -149,10 +151,10 @@ function setupExperimentMove(){
     }
 
     // make the same movement in the other tables
-    moveRowInOtherTables($row.closest('.tab-pane'), index, 'down');
+    moveRowInOtherTables($row.closest('.tab-pane'), $data_type, index, 'down');
 
     // update the row directions
-    updateTableIndex($(this).closest('.js-cocoon').find('.tab-pane table tbody'));
+    updateTableIndex($(this).closest('.js-cocoon').find('.tab-pane table[data-type="' + $data_type + '"] tbody'));
 
     return false;
   });
