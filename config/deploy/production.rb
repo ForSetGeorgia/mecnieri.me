f48788ec86667641d5d146da1f4c9840fdd0294b
jason <jason.addie@gmail.com>
2017-02-06 15:49:54 +0400

    switching to real domain for prod

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,5 +4,5 @@ set :application, 'Mecnieri'
 # easier to use https; if you use ssh then you have to create key on server
 set :repository, 'https://github.com/JumpStartGeorgia/metsnieri.me.git'
 set :branch, 'master'
-set :web_url, 'mecnieri.jumpstart.ge'
-set :use_ssl, true
+set :web_url, 'mecnieri.me'
+set :use_ssl, false
