2b62c75db51bf8faf4f053ef1f482c1727015f9b
meripepanashvili <mpepa13@freeuni.edu.ge>
2017-02-09 13:18:03 +0400

    loading on page change

--- app/assets/javascripts/application/loading_indicator.js
+++ app/assets/javascripts/application/loading_indicator.js
@@ -1,12 +1,16 @@
 // Shows and hides the loading indicator when users switch pages
 
+
 $(document).on("page:before-change", function() {
   $(".loading-indicator").fadeIn();
 });
 
-$(window).load(function() {
-	 $(".loading-indicator").fadeOut();
+$(document).on("page:change", function(){
+  $(".loading-indicator").fadeOut();
 });
+
+
+
 
 // $(document).on("page:load, page:change", function() {
 //   $(".loading-indicator").fadeOut();
