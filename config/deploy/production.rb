dcedbbe8b7dd53d6f00dd83d5cf64d0339efe344
jason <jason.addie@gmail.com>
2018-01-31 11:36:38 +0400

    change prod server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,12 +1,12 @@
-set :domain, 'pins.ge'
-set :user, 'mecnieri'
+set :domain, 'gamma.forset.ge'
+set :user, 'deploy'
 set :application, 'Mecnieri'
 # easier to use https; if you use ssh then you have to create key on server
-set :repository, 'https://github.com/JumpStartGeorgia/metsnieri.me.git'
+set :repository, 'https://github.com/ForSetGeorgia/mecnieri.me.git'
 set :branch, 'master'
 set :web_url, 'mecnieri.me'
 set :secondary_web_url, 'www.mecnieri.me'
-set :use_ssl, true
+set :use_ssl, false
 set :puma_worker_count, '2'
 set :puma_thread_count_min, '1'
 set :puma_thread_count_max, '8'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,9 +1,9 @@
-set :domain, 'alpha.jumpstart.ge'
-set :user, 'metsnieri-staging'
-set :application, 'Metsnieri'
+set :domain, 'alpha.forset.ge'
+set :user, 'deploy'
+set :application, 'Mecnieri'
 # easier to use https; if you use ssh then you have to create key on server
-set :repository, 'https://github.com/JumpStartGeorgia/metsnieri.me.git'
+set :repository, 'https://github.com/ForSetGeorgia/mecnieri.me.git'
 set :branch, 'master'
-set :web_url, 'dev-metsnieri.jumpstart.ge'
+set :web_url, 'dev-metsnieri.forset.ge'
 set :visible_to_robots, false
 set :use_ssl, true
